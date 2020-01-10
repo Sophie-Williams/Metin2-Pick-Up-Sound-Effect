7b8e88efff4e73893ccd1ee531c15bea4acda4e0
Muhammet <33325253+blackdragonx61@users.noreply.github.com>
2020-01-10 18:13:11 +0300

    Update PythonPlayer.cpp

--- 1.Svn/Client/UserInterface/PythonPlayer.cpp
+++ 1.Svn/Client/UserInterface/PythonPlayer.cpp
@@ -29,14 +29,16 @@
 		if (!CItemManager::Instance().GetItemDataPointer(CPythonItem::Instance().GetVirtualNumberOfGroundItem(dwIID), &pItemData))
 			return;
 		 // -1: all subtypes
+		 // don't forget to add #include <tuple>
 		const std::vector<std::tuple<BYTE, BYTE, std::string>> pickup {
 			{ CItemData::ITEM_TYPE_ELK, -1, "money.wav" },
 			{ CItemData::ITEM_TYPE_WEAPON, -1, "pickup_item_in_inventory.wav" },
 			{ CItemData::ITEM_TYPE_ARMOR, CItemData::ARMOR_BODY, "bracelet.wav" },
-			{ CItemData::ITEM_TYPE_ARMOR, CItemData::ARMOR_SHIELD, "money.wav" },
+			{ CItemData::ITEM_TYPE_ARMOR, CItemData::ARMOR_SHIELD, "drop.wav" },
+			{ CItemData::ITEM_TYPE_ARMOR, -1, "itemget.wav" }, // rest
 		};
 		std::string base = "sound/ui/";
-		const decltype(pickup)::const_iterator it = std::find_if(pickup.begin(), pickup.end(), [&pItemData](const std::tuple<BYTE, BYTE, std::string>& t) { return pItemData->GetType() == std::get<0>(t) && (pItemData->GetSubType() == std::get<1>(t) || -1 == std::get<1>(t));});
+		const decltype(pickup)::const_iterator it = std::find_if(pickup.begin(), pickup.end(), [&pItemData](const std::tuple<BYTE, BYTE, std::string>& t) { return pItemData->GetType() == std::get<0>(t) && (pItemData->GetSubType() == std::get<1>(t) || static_cast<BYTE>(-1) == std::get<1>(t));});
 		base += it != pickup.end() ? std::get<2>(*it) : "close.wav";
 		CSoundManager::Instance().PlaySound2D(base.c_str());
 		*/
